80680352c0b164788af6dae663cf902253000763
Maxim Maslin <zapzarap@yandex.ru>
2019-05-31 22:20:53 +0300

    add regex task solution

--- Программирование на Python Specialization/Создание веб-сервисов на Python/week1/regexp_sample/regexp.py
+++ Программирование на Python Specialization/Создание веб-сервисов на Python/week1/regexp_sample/regexp.py
@@ -1,7 +1,6 @@
 def calculate(data, findall):
-    matches = findall(r"([abc])([+-])?=([abc])?([+-]?\d+)?")  # Если придумать хорошую регулярку, будет просто
-    for a, sign, b, number in matches:  # Если кортеж такой структуры: var1, [sign]=, [var2], [[+-]number]
-        # Если бы могло быть только =, вообще одной строкой все считалось бы, вот так:
+    matches = findall(r"([abc])([+-]?)=([abc])?([+-]?\d+)?")
+    for a, sign, b, number in matches:
         right = data.get(b, 0) + int(number or 0)
         if sign == "-":
             data[a] -= right
@@ -9,5 +8,4 @@ def calculate(data, findall):
             data[a] += right
         else:
             data[a] = right
-
     return data
