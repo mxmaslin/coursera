54e4d8a3fbdcedef7a4e2b75ac6b976e79d4ba4f
Maxim Maslin <zapzarap@yandex.ru>
2019-07-14 21:38:36 +0300

    remove comment

--- Программирование на Python Specialization/Создание веб-сервисов на Python/week7/student/coursera_house/core/models.py
+++ Программирование на Python Specialization/Создание веб-сервисов на Python/week7/student/coursera_house/core/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class Setting(models.Model):
     controller_name = models.CharField(max_length=40, unique=True)
     label = models.CharField(max_length=100)
