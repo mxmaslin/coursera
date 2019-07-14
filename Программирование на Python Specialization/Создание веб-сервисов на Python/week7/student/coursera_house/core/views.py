9e400fb1db1ba9cd8f97701da708d2c37cff7c03
Maxim Maslin <zapzarap@yandex.ru>
2019-07-14 21:40:46 +0300

    add initial values to form

--- Программирование на Python Specialization/Создание веб-сервисов на Python/week7/student/coursera_house/core/views.py
+++ Программирование на Python Specialization/Создание веб-сервисов на Python/week7/student/coursera_house/core/views.py
@@ -7,6 +7,20 @@ from .form import ControllerForm
 from .tasks import poll_controller
 
 
+def get_or_update(controller_name, label, value):
+    try:
+        entry = Setting.objects.get(controller_name=controller_name)
+    except Setting.DoesNotExist:
+        Setting.objects.create(
+            controller_name=controller_name,
+            label=label,
+            value=value
+        )
+    else:
+        entry.value = value
+        entry.save()
+
+
 class ControllerView(FormView):
     form_class = ControllerForm
     template_name = 'core/control.html'
@@ -21,7 +35,33 @@ class ControllerView(FormView):
         return {}
 
     def form_valid(self, form):
-        return super(ControllerView, self).form_valid(form)
+        bedroom_target_temperature_value = form.cleaned_data['bedroom_target_temperature']
+        hot_water_target_temperature_value = form.cleaned_data['hot_water_target_temperature']
+        bedroom_light_value = form.cleaned_data['bedroom_light']
+        bathroom_light_value = form.cleaned_data['bathroom_light']
+
+        get_or_update(
+            'bedroom_target_temperature',
+            'Bedroom target temperature',
+            bedroom_target_temperature_value
+        )
+        get_or_update(
+            'hot_water_target_temperature',
+            'Hot water target temperature value',
+            hot_water_target_temperature_value
+        )
+        get_or_update(
+            'bedroom_light',
+            'Bedroom light',
+            bedroom_light_value
+        )
+        get_or_update(
+            'bathroom_light',
+            'Bathroom light',
+            bathroom_light_value
+        )
+        ss = Setting.objects.all()
+        return super(ControllerView, self).get(form)
 
 
 def controller_data(request):
