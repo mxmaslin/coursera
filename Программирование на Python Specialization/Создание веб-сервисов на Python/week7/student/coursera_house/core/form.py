29ff28482e3ab65768b37df99a834d676e79a5ed
Maxim Maslin <zapzarap@yandex.ru>
2019-07-14 21:38:13 +0300

    add validators

--- Программирование на Python Specialization/Создание веб-сервисов на Python/week7/student/coursera_house/core/form.py
+++ Программирование на Python Specialization/Создание веб-сервисов на Python/week7/student/coursera_house/core/form.py
@@ -1,8 +1,15 @@
 from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class ControllerForm(forms.Form):
-    bedroom_target_temperature = forms.IntegerField()
-    hot_water_target_temperature = forms.IntegerField()
-    bedroom_light = forms.BooleanField()
-    bathroom_light = forms.BooleanField()
+    bedroom_target_temperature = forms.IntegerField(
+        required=False,
+        validators=[MinValueValidator(16), MaxValueValidator(50)]
+    )
+    hot_water_target_temperature = forms.IntegerField(
+        required=False,
+        validators=[MinValueValidator(24), MaxValueValidator(90)]
+    )
+    bedroom_light = forms.BooleanField(required=False)
+    bathroom_light = forms.BooleanField(required=False)
