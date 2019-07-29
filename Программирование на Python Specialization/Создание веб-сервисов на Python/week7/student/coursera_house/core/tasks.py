c6b76cbf03e5c3daac80ec905708e25a3d004d6c
Maxim Maslin <zapzarap@yandex.ru>
2019-07-29 10:42:36 +0300

    add comments

--- Программирование на Python Specialization/Создание веб-сервисов на Python/week7/student/coursera_house/core/tasks.py
+++ Программирование на Python Specialization/Создание веб-сервисов на Python/week7/student/coursera_house/core/tasks.py
@@ -18,8 +18,10 @@ def smart_home_manager():
     payload = {
         'controllers': []
     }
-    water_leaks = controller_data['leak_detector']['value']
-    if water_leaks:
+
+    if controller_data['leak_detector']['value']:
+        # если датчик показывает протечку и есть гор. и/или хол. вода,
+        # перекрываем гор. и/или хол. воду
         if controller_data['cold_water']['value']:
             payload['controllers'].append({'name': 'cold_water', 'value': False})
         if controller_data['hot_water']['value']:
@@ -32,18 +34,18 @@ def smart_home_manager():
         )
         email.send(fail_silently=False)
 
-    waching_machine_status = controller_data['washing_machine']['value']
+    # если нет холодной воды и бойлер и/или стиралка включены или сломаны - выключаем их
     if not controller_data['cold_water']['value']:
         if controller_data['boiler']['value']:
             payload['controllers'].append({'name': 'boiler', 'value': False})
-        if waching_machine_status in ('on', 'broken'):
+        if controller_data['washing_machine']['value'] in ('on', 'broken'):
             payload['controllers'].append({'name': 'washing_machine', 'value': "off"})
 
     boiler_temperature = controller_data['boiler_temperature']['value']
     hot_water_target_temperature = Setting.objects.get(
         controller_name='hot_water_target_temperature').value
     hot_water_low_temp = hot_water_target_temperature - hot_water_target_temperature * 0.1
-    hot_water_ok_temp = hot_water_target_temperature + hot_water_target_temperature * 0.1
+    hot_water_hi_temp = hot_water_target_temperature + hot_water_target_temperature * 0.1
 
     bedroom_temperature = controller_data['bedroom_temperature']['value']
     bedroom_target_temperature = Setting.objects.get(
@@ -51,9 +53,8 @@ def smart_home_manager():
     bedroom_low_temp = bedroom_target_temperature - bedroom_target_temperature * 0.1
     bedroom_high_temp = bedroom_target_temperature + bedroom_target_temperature * 0.1
 
-    smoke_detector = controller_data['smoke_detector']['value']
-
-    if smoke_detector:
+    if controller_data['smoke_detector']['value']:
+        # если дым, выключаем кондиционер, бойлер и свет, но только если они включены
         if controller_data['air_conditioner']['value']:
             payload['controllers'].append(
                 {'name': 'air_conditioner', 'value': False}
@@ -73,20 +74,22 @@ def smart_home_manager():
                 {'name': 'washing_machine', 'value': 'off'}
             )
     else:
+        # если дыма нет
         if controller_data['cold_water']['value']:
+            # если есть хол. вода, греем или выключаем бойлер
             if boiler_temperature < hot_water_low_temp:
                 payload['controllers'].append({'name': 'boiler', 'value': True})
-            elif boiler_temperature > hot_water_ok_temp:
+            elif boiler_temperature > hot_water_hi_temp:
                 payload['controllers'].append({'name': 'boiler', 'value': False})
+        # регулируем комнатную температуру
         if bedroom_temperature > bedroom_high_temp:
             payload['controllers'].append({'name': 'air_conditioner', 'value': True})
         elif bedroom_temperature < bedroom_low_temp:
             payload['controllers'].append({'name': 'air_conditioner', 'value': False})
 
-    curtains = controller_data['curtains']['value']
     outdoor_light = controller_data['outdoor_light']['value']
     bedroom_light = controller_data['bedroom_light']['value']
-    if curtains == 'slightly_open':
+    if controller_data['curtains']['value'] == 'slightly_open':
         pass
     else:
         if outdoor_light < 50 and not bedroom_light:
@@ -94,4 +97,9 @@ def smart_home_manager():
         elif outdoor_light > 50 or bedroom_light:
             payload['controllers'].append({'name': 'curtains', 'value': 'close'})
     if payload['controllers']:
+        unique = []
+        for item in payload['controllers']:
+            if item not in unique:
+                unique.append(item)
+        payload['controllers'] = unique
         requests.post(url, headers=headers, json=payload)
