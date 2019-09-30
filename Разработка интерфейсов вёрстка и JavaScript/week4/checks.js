8492abc1c7ffdb4f45a6fa70bcee0b76bb365370
Maxim Maslin <zapzarap@yandex.ru>
2019-09-30 11:31:21 +0300

    add week 4 solution 6/10

---
+++ Разработка интерфейсов вёрстка и JavaScript/week4/checks.js
@@ -0,0 +1,71 @@
+// Встроенный в Node.JS модуль для проверок
+var assert = require('assert');
+
+// Подключаем свою функцию
+var lib = require('./index.js');
+
+// Коллекция данных
+var friends = [
+    {
+        name: 'Сэм',
+        gender: 'Мужской',
+        email: 'luisazamora@example.com',
+        favoriteFruit: 'Картофель'
+    },
+    {
+        name: 'Эмили',
+        gender: 'Женский',
+        email: 'example@example.com',
+        favoriteFruit: 'Яблоко'
+    },
+    {
+        name: 'Мэт',
+        gender: 'Мужской',
+        email: 'danamcgee@example.com',
+        favoriteFruit: 'Яблоко'
+    },
+    {
+        name: 'Брэд',
+        gender: 'Мужской',
+        email: 'newtonwilliams@example.com',
+        favoriteFruit: 'Банан'
+    },
+    {
+        name: 'Шерри',
+        gender: 'Женский',
+        email: 'danamcgee@example.com',
+        favoriteFruit: 'Картофель'
+    },
+    {
+        name: 'Керри',
+        gender: 'Женский',
+        email: 'danamcgee@example.com',
+        favoriteFruit: 'Апельсин'
+    },
+    {
+        name: 'Стелла',
+        gender: 'Женский',
+        email: 'waltersguzman@example.com',
+        favoriteFruit: 'Картофель'
+    }
+];
+
+// Выполняем выборку и фильтрацию с помощью нашего конструктора
+var result = lib.query(
+    friends,
+    lib.select('name', 'gender', 'email'),
+    lib.filterIn('favoriteFruit', ['Яблоко', 'Картофель'])
+);
+
+console.log(result);
+
+// Сравниваем полученный результат с ожидаемым
+assert.deepEqual(result, [
+    { name: 'Сэм', gender: 'Мужской', email: 'luisazamora@example.com' },
+    { name: 'Эмили', gender: 'Женский', email: 'example@example.com' },
+    { name: 'Мэт', gender: 'Мужской', email: 'danamcgee@example.com' },
+    { name: 'Шерри', gender: 'Женский', email: 'danamcgee@example.com' },
+    { name: 'Стелла', gender: 'Женский', email: 'waltersguzman@example.com' }
+]);
+
+console.info('OK!');
---
+++ Разработка интерфейсов вёрстка и JavaScript/week4/index.js
@@ -0,0 +1,141 @@
+/**
+ * @param {Array} collection
+ * @params {Function[]} – Функции для запроса
+ * @returns {Array}
+ */
+function query(collection) {
+    var friends = JSON.parse(JSON.stringify(collection));
+
+	if (arguments.length === 1) return friends;
+
+    // filterIn
+    for (var i = 1; i < arguments.length; i++){
+        if (arguments[i].name == 'filterIn') {
+            var f = arguments[i];
+            var filtered = f(friends);
+            friends = intersect(friends, filtered);
+        }     
+    }
+
+    // select
+    var selectArgs = [];
+    for (var i = 1; i < arguments.length; i++){
+        if (arguments[i].name == 'select') {
+            var f = arguments[i];
+            selectArgs.push(f(collection));
+        }
+    }
+    var intersectedSelectArgs = getIntersectedSelectArgs(selectArgs);
+    friends = selectIntersected(friends, intersectedSelectArgs);    
+
+    return friends;
+}
+
+function intersect(array1, array2) {
+    return array1.filter(function(element){
+        return array2.indexOf(element) != -1;
+    });
+}
+
+function getIntersectedSelectArgs(args){
+    return args.reduce(function(a, b){
+        return a.filter(function(element){
+            return b.indexOf(element) != -1;
+        });
+    });
+}
+
+/**
+ * @params {String[]}
+ */
+function select() {
+    var args = [].slice.call(arguments);
+    return function select(collection) {
+        return args;
+    }
+}
+
+function selectIntersected(collection, fields) {
+    var friends = [];
+    for (var item in collection){
+        var friend = {};
+        for (var field in fields){
+            friend[fields[field]] = collection[item][fields[field]];
+        }
+        friends.push(friend);
+    }
+    return friends;
+}
+
+/**
+ * @param {String} property – Свойство для фильтрации
+ * @param {Array} values – Массив разрешённых значений
+ */
+function filterIn(property, values) {
+    return function filterIn(collection){
+        return collection.filter(function(n){
+            return values.indexOf(n[property]) != -1;
+        });
+    }
+}
+
+module.exports = {
+    query: query,
+    select: select,
+    filterIn: filterIn
+};
+
+// var friends = [
+//     {
+//         name: 'Сэм',
+//         gender: 'Мужской',
+//         email: 'luisazamora@example.com',
+//         favoriteFruit: 'Картофель'
+//     },
+//     {
+//         name: 'Эмили',
+//         gender: 'Женский',
+//         email: 'example@example.com',
+//         favoriteFruit: 'Яблоко'
+//     },
+//     {
+//         name: 'Мэт',
+//         gender: 'Мужской',
+//         email: 'danamcgee@example.com',
+//         favoriteFruit: 'Яблоко'
+//     },
+//     {
+//         name: 'Брэд',
+//         gender: 'Мужской',
+//         email: 'newtonwilliams@example.com',
+//         favoriteFruit: 'Банан'
+//     },
+//     {
+//         name: 'Шерри',
+//         gender: 'Женский',
+//         email: 'danamcgee@example.com',
+//         favoriteFruit: 'Картофель'
+//     },
+//     {
+//         name: 'Керри',
+//         gender: 'Женский',
+//         email: 'danamcgee@example.com',
+//         favoriteFruit: 'Апельсин'
+//     },
+//     {
+//         name: 'Стелла',
+//         gender: 'Женский',
+//         email: 'waltersguzman@example.com',
+//         favoriteFruit: 'Картофель'
+//     }
+// ];
+
+// // Выполняем выборку и фильтрацию с помощью нашего конструктора
+// var result = query(
+//     friends,
+//     select('name', 'gender', 'email'),
+//     filterIn('favoriteFruit', ['Яблоко', 'Картофель']),
+//     filterIn('favoriteFruit', ['Картофель']),
+//     select('name', 'gender')
+// );
+// console.log(result);
